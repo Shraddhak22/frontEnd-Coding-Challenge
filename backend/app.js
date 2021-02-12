4ac9dff0a3b361d699fb7bd5ec72f26c05d5ab96
shraddhak22 <spkharche22@gmail.com>
2021-02-12 16:23:34 -0500

    Front end coding challenge

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const port = 4000
+
+const data = require('./cards.json')
+console.log(data)
+app.get('/getCards', function (req, res) {
+  res.header("Content-Type",'application/json');
+  res.send(JSON.stringify(data));
+})
+
+app.listen(port, () =>{
+    console.log(`listening on ${port}`)
+})
